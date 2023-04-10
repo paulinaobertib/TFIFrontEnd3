3f9758cf4fc9fb3f25c6ccde6b1e9bc4a9f08f2d
Marianaca-29 <mariana.famaf@gmail.com>
2023-04-10 17:19:07 -0300

    test fixed

--- src/Pages/login/test/Login.test.jsx
+++ src/Pages/login/test/Login.test.jsx
@@ -2,13 +2,20 @@ import { describe, it, expect } from "vitest";
 import { render, screen, fireEvent } from '@testing-library/react'
 import matchers from "@testing-library/jest-dom/matchers";
 import Login from '../Login';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 expect.extend(matchers);
 
 describe("Test del componente Login", () => {
     it("Se renderiza el componente en el dom", () => {
-      render(<Login />)
-      const EmailInput = screen.getByPlaceholderText("Ingresa tu email");
+      render(
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+        </Routes> 
+      </BrowserRouter>
+      )
+      const EmailInput = screen.getByPlaceholderText("Ingrese su correo");
       expect(EmailInput).toBeDefined();
       expect(EmailInput.value).toEqual("");
       fireEvent.change(EmailInput, { target: { value: "marianapaulina@dh.com" } })
